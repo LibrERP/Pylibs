1c518f30b79f208e7b23c7e221cd013016da583a
Marco Tosato <marco.tosato@didotech.com>
2022-09-16 11:15:54 +0200

    [IMP + REF] tasks/anagrafiche/tasks.py - modifiche varie, vedi commento commit
    - Refactoring metodi di lettura dati Fornitore per inserirli nella classe SuppliersReader
    - Aggiunto logging per connessione del writer
    - Gestione chiavi multi livello (mediante tuple) per scrittura dati nel form
    - Aggiunta classe AnagraficheID per leggere l'ID dell'ultimo record creato per clienti e fornitori

--- av2000_terminator/tasks/anagrafiche/tasks.py
+++ av2000_terminator/tasks/anagrafiche/tasks.py
@@ -15,86 +15,84 @@ from ...navigation.base.form import Field
 from ...navigation.pages.contabilita.anagrafiche.fornitori import ListaFornitori
 
 
-def suppliers_list(connection_params: ConnectionParams):
+class SuppliersReader:
 
-    # Number of items read
-    counter = 0
-    trigger_print = 100
+    def __init__(self, connection_params: ConnectionParams):
+        self._connection_params = connection_params
+    # end __init__
 
-    # Build the driver object
-    av2000: AV2000Terminal = AV2000Terminal(connection_params)
+    def list_suppliers(self):
 
-    # Get a navigator
-    navigator: Navigator = Navigator(av2000)
+        # Number of items read
+        counter = 0
+        trigger_print = 100
 
-    # Load suppliers list page
-    navigator.current_page.menu_select(3)  # contabilita.MainMenu
-    navigator.current_page.menu_select(9)  # AnagraficaFornitori
-    navigator.current_page.open_partners_list()  # ListaFornitori
+        # Build the driver object
+        av2000: AV2000Terminal = AV2000Terminal(self._connection_params)
 
-    current_page: ListaFornitori = navigator.current_page
+        # Get a navigator
+        navigator: Navigator = Navigator(av2000)
 
-    print('Suppliers list - Start downloading process...')
+        # Load suppliers list page
+        navigator.current_page.menu_select(3)  # contabilita.MainMenu
+        navigator.current_page.menu_select(9)  # AnagraficaFornitori
+        navigator.current_page.open_partners_list()  # ListaFornitori
 
-    try:
-        while True:
-            page_data = current_page.extract_data()
+        current_page: ListaFornitori = navigator.current_page
 
-            counter += len(page_data)
-            if counter > trigger_print:
-                trigger_print += 100
-                print(f'Suppliers list - Downloaded {counter} items.')
-            # end if
+        print('Suppliers list - Start downloading process...')
 
-            for row in page_data:
-                yield row
-            # end for
+        try:
+            while True:
+                page_data = current_page.extract_data()
 
-            current_page.scroll_next()
-        # end while
+                counter += len(page_data)
+                if counter > trigger_print:
+                    trigger_print += 100
+                    print(f'Suppliers list - Downloaded {counter} items.')
+                # end if
 
-    except NoNextPage:
-        # End of data reached
-        print(f'Suppliers list - Download completed. Total items: {counter}')
-    # end try / except
+                for row in page_data:
+                    yield row
+                # end for
 
-    navigator.exit()
-# end suppliers_list
+                current_page.scroll_next()
+            # end while
 
+        except NoNextPage:
+            # End of data reached
+            print(f'Suppliers list - Download completed. Total items: {counter}')
+        # end try / except
 
-def suppliers_get_batch(
-        connection_params: ConnectionParams, ids_list, procs_num: int = 1
-) -> ParallelProducer:
-    return ParallelProducer(
-        connection_params=connection_params,
-        worker_class=SuppliersDownloader,
-        tasks_list=ids_list,
-        procs_num=procs_num,
-    )
-# end suppliers_get_batch
+        navigator.exit()
+    # end suppliers_list
 
+    def get_suppliers_producer(self, ids_list: typing.List = None, procs_num: int = 1) -> ParallelProducer:
+        '''
+            Returns a list of suppliers ordered by AV2000 id from the smallest id to the largest id,
+            this should be equivalent to ordering from the oldest record to the most recent one,
+            this way if a record collides with another the most recent version is actually imported.
 
-def suppliers_get_all(
-        connection_params: ConnectionParams, procs_num: int = 1
-) -> ParallelProducer:
-    '''
-        Return a list of suppliers ordered by AV2000 id from the smallest id to the largest id,
-        this should be equivalent to ordering from the oldest record to the most recent one,
-        this way if a record collides with another the most recent version is actually imported.
-    '''
+            If ids_list
+        '''
 
-    all_suppliers = list(suppliers_list(connection_params))
-    all_suppliers.sort(key=lambda r: r['codice'])
+        # Dowload list of all suppliers if parameter ids_list is not specified
+        if ids_list in (None, False):
+            ids_list = list(self.list_suppliers())
+            ids_list.sort(key=lambda r: r['codice'])
 
-    # Solo per test rapidi
-    # all_suppliers = all_suppliers[:48]
+            # Solo per test rapidi
+            # ids_list = all_suppliers[:48]
+        # end if
 
-    return suppliers_get_batch(
-        connection_params=connection_params,
-        ids_list=all_suppliers,
-        procs_num=procs_num,
-    )
-# end suppliers_get_batch
+        return ParallelProducer(
+            connection_params=self._connection_params,
+            worker_class=SuppliersDownloader,
+            tasks_list=ids_list,
+            procs_num=procs_num,
+        )
+    # end get_suppliers
+# end SuppliersReader
 
 
 class SuppliersWriter:
@@ -125,9 +123,13 @@ class SuppliersWriter:
             self._av2000 = AV2000Terminal(self._connection_params)
             self._navigator = Navigator(self._av2000)
 
+            self._logger.info('Back to main menu')
             self._navigator.back_to_main_menu()
+            self._logger.info('Open "Archivi di base area contabile"')
             self._navigator.current_page.menu_select(3)
+            self._logger.info('Open "Anagrafica fornitori"')
             self._navigator.current_page.menu_select(9)
+            self._logger.info('Writer ready!')
         # end if
 
     # end connect
@@ -149,7 +151,23 @@ class SuppliersWriter:
         # Write data
         self._logger.info(f'[supplier {sc:>5s}] Setting values for fields')
         for key, value in data.items():
-            self._navigator.current_page.fields[key].data = value
+
+            if isinstance(key, (int, str)):
+                # Single level key
+                tgt_field = self._navigator.current_page.fields[key]
+            elif isinstance(key, tuple):
+                # Multi level key
+                pointer = self._navigator.current_page.fields
+                for key_part in key:
+                    pointer = pointer[key_part]
+                # end for
+
+                tgt_field = pointer
+            else:
+                assert False
+            # end if
+
+            tgt_field.data = value
         # end for
 
         # Save and close page
@@ -176,4 +194,76 @@ class SuppliersWriter:
         self._av2000.send_seq(keys.END)
         self._av2000.send_seq(keys.END)
     # end exit_on_error
+# end SuppliersWriter
+
+
+class AnagraficheID:
+    """
+    Ritorna gli ID di AV2000 dell'ultimo record creato in AV2000 per clienti e fornitori
+    """
+
+    def __init__(self, connection_params: ConnectionParams):
+
+        class_obj = self.__class__
+        self._logger = logging.getLogger(f'{class_obj.__module__}.{class_obj.__qualname__}')
+
+        self._connection_params = connection_params
+
+        self._av2000: AV2000Terminal = None
+        self._navigator: Navigator = None
+    # end __init__
+
+    @property
+    def connected(self):
+        if self._av2000 and not self._av2000.closed:
+            return True
+        else:
+            return False
+        # end if
+    # end connected
+
+    def connect(self):
+
+        if not self.connected:
+            self._av2000 = AV2000Terminal(self._connection_params)
+            self._navigator = Navigator(self._av2000)
+
+            self._logger.info('Back to main menu')
+            self._navigator.back_to_main_menu()
+            self._logger.info('Open "Archivi di base area contabile"')
+            self._navigator.current_page.menu_select(3)
+            self._logger.info('Ready to retrieve ids!')
+        # end if
+
+    # end connect
+
+    def close(self):
+        if self.connected:
+            self._navigator.exit()
+        # end if
+    # end close
+
+    def get_ids(self):
+
+        # Ensure we are connected to AV2000
+        self.connect()
+
+        # Open page
+        self._logger.info('Open "Progressivi base contabilità"')
+        self._navigator.current_page.menu_select(3)
+
+        # Retrieve data
+        self._logger.info('Getting IDs')
+        last_used_ids = {
+            'client': self._navigator.current_page.max_client_id,
+            'supplier': self._navigator.current_page.max_supplier_id,
+        }
+
+        # Back to menu
+        self._logger.info('Back to menu "Archivi di base area contabile"')
+        self._navigator.back()
+
+        # Return  data
+        return last_used_ids
+    # end get_ids
 # end SuppliersWriter
